43de976579d1510668a4c43dff2f224887f9889a
vladimirbutyrskii <vlaver@mail.ru>
2026-05-05 23:47:52 +0300

    addind pagination (HabitPagination, 5 per page)

--- habits/views.py
+++ habits/views.py
@@ -3,12 +3,15 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Habit
 from .serializers import HabitSerializer, HabitCreateSerializer, HabitUpdateSerializer
 from .permissions import IsOwner
+from .pagination import HabitPagination
 
 
 class HabitViewSet(viewsets.ModelViewSet):
     """CRUD для привычек текущего пользователя."""
 
+    serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated, IsOwner]
+    pagination_class = HabitPagination
 
     def get_serializer_class(self):
         if self.action == 'create':
@@ -31,6 +34,7 @@ class PublicHabitListView(generics.ListAPIView):
 
     serializer_class = HabitSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = HabitPagination
 
     def get_queryset(self):
         """Только публичные привычки, исключая привычки текущего пользователя."""
